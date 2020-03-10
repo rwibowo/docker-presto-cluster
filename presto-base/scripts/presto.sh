8d768413fa4a8146828957717cf399a6b37f1971
Wibowo Rio <rio.wibowo@nokia.com>
2020-03-10 17:59:17 +0200

    docker swarm support with scale support

--- presto-base/scripts/presto.sh
+++ presto-base/scripts/presto.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-node_id=$1
+node_id=${1:-coordinator}
+if [ "$node_id" == "worker" ]
+then
+  node_id=$(uuid)
+fi
+
 python /usr/local/presto/scripts/render.py --node-id $node_id etc/node.properties.template
 /usr/local/presto/bin/launcher run
-
-
-
-
